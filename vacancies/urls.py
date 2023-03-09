d674317619d5dcc852a57db08519824aec2259ba
GG <sudodebug@gmail.com>
2023-03-09 15:13:06 +0500

    Added VacanciesListView

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from vacancies.views import index
-
+from vacancies.views import index, VacanciesListView
 
 urlpatterns = [
     path('', index),
-]
+    path('vacancies/', VacanciesListView.as_view())
+    ]
--- vacancies/views.py
+++ vacancies/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from vacancies.models import Vacancies
 
 
 def index(request):
     return render(request, 'vacancies/index.html')
+
+
+class VacanciesListView(ListView):
+    model = Vacancies
+    template_name = 'vacancies/vacancies.html'
