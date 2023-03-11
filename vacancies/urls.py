bc1ce3f4140246595ab517a22b14e60b52fe883b
GG <sudodebug@gmail.com>
2023-03-11 13:08:03 +0500

    Create company path

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from vacancies.views import index, VacanciesListView
+from vacancies.views import index, VacanciesListView, CompaniesDetailView
 
 urlpatterns = [
     path('', index),
-    path('vacancies/', VacanciesListView.as_view())
+    path('vacancies/', VacanciesListView.as_view(), name='vacancies'),
+    path('companies/<int:pk>/', CompaniesDetailView.as_view(), name='company'),
     ]
