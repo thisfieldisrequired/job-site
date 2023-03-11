d6b0d2bb6ed089145a113d9b32982a4141d97fa4
GG <sudodebug@gmail.com>
2023-03-11 13:10:14 +0500

    Create vacancy path

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from vacancies.views import index, VacanciesListView, CompaniesDetailView
+from vacancies.views import index, VacanciesListView, CompaniesDetailView, VacanciesDetailView
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='main'),
     path('vacancies/', VacanciesListView.as_view(), name='vacancies'),
+    path('vacancies/<int:pk>/', VacanciesDetailView.as_view(), name='vacancy'),
     path('companies/<int:pk>/', CompaniesDetailView.as_view(), name='company'),
     ]
